9c4d807d87fe34ad59198bc879cfda6bed78d6a7
bwhite03 <bwhite1195@yahoo.com>
2019-07-14 15:24:05 -0400

    disabled all buttons when user enters room

--- public/chat.js
+++ public/chat.js
@@ -9,44 +9,80 @@ const message = document.getElementById("message"),
   feedback = document.getElementById("feedback"),
   userCount = document.getElementById("user-count"),
   joinChatRoom = document.getElementById("join-chat-room"),
+  styleChatRoom = document.getElementById("chat-room"),
   leaveChatRoom = document.getElementById("leave-chat-room"),
   joinGamingRoom = document.getElementById("join-gaming-room"),
+  stylegamingRoom = document.getElementById("gaming-room"),
   leaveGamingRoom = document.getElementById("leave-gaming-room"),
   joinDevRoom = document.getElementById("join-dev-room"),
+  styleDevRoom = document.getElementById("dev-room"),
   leaveDevRoom = document.getElementById("leave-dev-room"),
   joinMusicRoom = document.getElementById("join-music-room"),
-  leaveMusicRoom = document.getElementById("leave-music-room");
+  styleMusicRoom = document.getElementById("music-room"),
+  leaveMusicRoom = document.getElementById("leave-music-room"),
+  selectRoomTitle = document.getElementById("select-room-title"),
+  allButtons = document.querySelectorAll(".button");
+
+// Events
 
 // Join / Leave chat
 joinChatRoom.addEventListener("click", () => {
   socket.emit("join", "chat");
+  styleChatRoom.style.background = "#E5E5E5";
+  selectRoomTitle.innerHTML = "<h1>Chat room</h1>";
+  allButtons.forEach(button => (button.disabled = true));
+  leaveChatRoom.disabled = false;
 });
 leaveChatRoom.addEventListener("click", () => {
   socket.emit("leave", "chat");
+  styleChatRoom.style.background = "#fff";
+  selectRoomTitle.innerHTML = "<h1>Select room</h1>";
+  allButtons.forEach(button => (button.disabled = false));
 });
 // Join / Leave gaming
 joinGamingRoom.addEventListener("click", () => {
   socket.emit("join", "gaming");
+  stylegamingRoom.style.background = "#E5E5E5";
+  selectRoomTitle.innerHTML = "<h1>Gaming room</h1>";
+  allButtons.forEach(button => (button.disabled = true));
+  leaveGamingRoom.disabled = false;
 });
 leaveGamingRoom.addEventListener("click", () => {
   socket.emit("leave", "gaming");
+  stylegamingRoom.style.background = "#fff";
+  selectRoomTitle.innerHTML = "<h1>Select room</h1>";
+  allButtons.forEach(button => (button.disabled = false));
 });
 // Join / Leave dev
 joinDevRoom.addEventListener("click", () => {
   socket.emit("join", "dev");
+  styleDevRoom.style.background = "#E5E5E5";
+  selectRoomTitle.innerHTML = "<h1>Dev room</h1>";
+  allButtons.forEach(button => (button.disabled = true));
+  leaveDevRoom.disabled = false;
 });
 leaveDevRoom.addEventListener("click", () => {
   socket.emit("leave", "dev");
+  styleDevRoom.style.background = "#fff";
+  selectRoomTitle.innerHTML = "<h1>Select room</h1>";
+  allButtons.forEach(button => (button.disabled = false));
 });
 // Join / Leave music
 joinMusicRoom.addEventListener("click", () => {
   socket.emit("join", "music");
+  styleMusicRoom.style.background = "#E5E5E5";
+  selectRoomTitle.innerHTML = "<h1>Music room</h1>";
+  allButtons.forEach(button => (button.disabled = true));
+  leaveMusicRoom.disabled = false;
 });
 leaveMusicRoom.addEventListener("click", () => {
   socket.emit("leave", "music");
+  styleMusicRoom.style.background = "#fff";
+  selectRoomTitle.innerHTML = "<h1>Select room</h1>";
+  allButtons.forEach(button => (button.disabled = false));
 });
 
-// Events
+// Send Message Event
 btn.addEventListener("click", () => {
   if (handle.value === "") {
     handle.value = "Anonymous";
